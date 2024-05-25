7d22d4f9ac3a698a16e1d85fe29feb146a9cf0eb
Thibaut Stachnick <ttst@MacBook-Pro-de-Thibaut-2.local>
2024-05-25 20:19:05 +0200

    submit answer for leet problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val
+        }
+    }
+}
+
+pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    head.map(|mut ln|{
+        let mut current = ln.as_mut();
+        while let Some(next) = current.next.as_mut(){
+            if current.val == next.val {
+                current.next = next.next.take()
+            }else {
+                current = current.next.as_mut().unwrap();
+            }
+        }
+        ln
+    })
+}
+
 fn main() {
-    println!("Hello World")
+    let head:Option<Box<ListNode>> = Some(Box::new(ListNode::new(1)));
+    delete_duplicates(head);
 }
